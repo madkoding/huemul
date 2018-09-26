8de0625fdebac392e635ee065ea97f9af60566e5
juanbrujo <juanbrujo@gmail.com>
2018-09-26 20:45:17 -0300

    new(script)
    test: `huemul dame un backend`

---
+++ scripts/dameunbackend.js
@@ -0,0 +1,31 @@
+// Description:
+//   Muestra un clásico backend al azar
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Command:
+//   huemul dame un backend
+//
+// Author:
+//   @jorgeepunan
+
+const images = [
+  'https://i.imgur.com/3e93b3i.jpg',
+  'https://i.imgur.com/Ov018QN.png',
+  'https://i.imgur.com/uRM5nG7.jpg',
+  'https://i.imgur.com/FNla8KM.jpg',
+  'https://i.imgur.com/4V08TOr.jpg',
+  'https://i.imgur.com/zhLoqXK.jpg',
+  'https://i.imgur.com/Gvrhzwl.jpg',
+  'https://i.imgur.com/fJ3qWPU.jpg',
+  'https://i.imgur.com/3dQVdFo.jpg',
+  'https://i.imgur.com/JceLGX6.jpg'
+]
+
+module.exports = robot => {
+  robot.respond(/dame un backend/gi, msg => msg.send(msg.random(images)))
+}
