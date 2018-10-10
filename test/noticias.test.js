1555b3109658a165d00cc292aeaf824c7369fd45
Leonardo Gatica <lgatica@protonmail.com>
2018-10-10 17:00:53 -0300

    feat(noticias): change source to ahoranoticias
    Closes #437

--- test/noticias.test.js
+++ test/noticias.test.js
@@ -2,6 +2,7 @@ import 'coffee-script/register';
 import test from 'ava';
 import Helper from 'hubot-test-helper';
 import nock from 'nock'
+import querystring from 'querystring'
 
 const helper = new Helper('../scripts/noticias.js');
 const sleep = m => new Promise(resolve => setTimeout(() => resolve(), m));
@@ -13,9 +14,8 @@ test.beforeEach(t => {
 test.afterEach(t => t.context.room.destroy());
 
 test('Debe entregar mensaje de error', async t => {
-  nock('http://search.24horas.cl')
-    .get('/search/')
-    .query({q: 'game of thrones'})
+  nock('http://www.ahoranoticias.cl')
+    .post('/buscador/', querystring.stringify({ q: 'game of thrones', ajax: true }))
     .reply(200, 'Code ID: 503')
   t.context.room.user.say('user', 'hubot noticias game of thrones');
   await sleep(500);
@@ -26,6 +26,6 @@ test('Debe entregar mensaje de error', async t => {
   t.deepEqual(user, ['user', 'hubot noticias game of thrones']);
   t.deepEqual(hubot, [
     'hubot',
-    '@user ocurrió un error con la búsqueda'
+    ':huemul: *News*\nNo se han encontrado noticias sobre *game of thrones*.'
   ])
 });
