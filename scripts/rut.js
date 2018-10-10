73e12819f09953fbe857df6c80ae6fd422fd7b07
Leonardo Gatica <lgatica@protonmail.com>
2018-10-10 15:26:10 -0300

    feat(rut): add `persona` and `empresa`
    Closes #404

--- scripts/rut.js
+++ scripts/rut.js
@@ -8,20 +8,30 @@
 //   None
 //
 // Commands:
-//   hubot dame un rut
+//   hubot dame un rut persona
+//   hubot dame un rut empresa
 //
 // Author:
 //   @jorgeepunan
 
 var Rut = require('rutjs')
-var generar = function() {
-  var num_aleatorio = Math.round(Math.random() * (25000000 - 5000000)) + 5000000
+var generar = function(type) {
+  var min
+  var max
+  if (type === 'empresa') {
+    min = 50000000
+    max = 90000000
+  } else {
+    min = 5000000
+    max = 25000000
+  }
+  var num_aleatorio = Math.round(Math.random() * (max - min)) + min
   var rut = new Rut(num_aleatorio.toString(), true)
   return rut.getNiceRut()
 }
 
 module.exports = function(robot) {
-  robot.respond(/dame un rut/i, function(res) {
-    res.send('Un RUT: ' + generar())
+  robot.respond(/dame un rut (persona|empresa)$/i, function(res) {
+    res.send('Un RUT: ' + generar(res.match[1]))
   })
 }
