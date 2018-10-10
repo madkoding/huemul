a347decbc3cfa8841c48eb5e9f225762f19520d7
Leonardo Gatica <lgatica@protonmail.com>
2018-10-10 14:41:25 -0300

    style(rut): fix format with prettier

--- scripts/rut.js
+++ scripts/rut.js
@@ -13,17 +13,15 @@
 // Author:
 //   @jorgeepunan
 
-var Rut     = require("rutjs");
-var generar = function(){
-  var num_aleatorio = Math.round(Math.random()*(25000000-5000000))+5000000;
-  var rut = new Rut(num_aleatorio.toString(), true);
+var Rut = require('rutjs')
+var generar = function() {
+  var num_aleatorio = Math.round(Math.random() * (25000000 - 5000000)) + 5000000
+  var rut = new Rut(num_aleatorio.toString(), true)
   return rut.getNiceRut()
 }
 
 module.exports = function(robot) {
-
   robot.respond(/dame un rut/i, function(res) {
-    res.send( "Un RUT: " + generar() );
-  });
-
-};
+    res.send('Un RUT: ' + generar())
+  })
+}
