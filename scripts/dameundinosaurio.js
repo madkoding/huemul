92f007041e3b6fe072ce9672bc42180994069e1d
Jorge Riveros <jorge.riveros@itfb.cl>
2018-10-12 16:33:50 -0300

    Added new file for huemul dame un saurio

---
+++ scripts/dameundinosaurio.js
@@ -0,0 +1,38 @@
+// Description:
+//   Muestra un dinosaurio al azar
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Command:
+//   huemul dame un saurio
+//
+// Author:
+//   @jriveros
+
+const images = [
+    'https://i.imgur.com/a/M02kVmi',
+    'https://i.imgur.com/a/gY1bS2M',
+    'https://i.imgur.com/a/Q7QibR2',
+    'https://imgur.com/a/3Gq1LYN',
+    'https://imgur.com/a/7IpKONP',
+    'https://imgur.com/a/CinB8DY',
+    'https://imgur.com/9GKaYjd',
+    'https://imgur.com/XCG5pCP',
+    'https://imgur.com/A3waVPT',
+    'https://imgur.com/uvVKy3B',
+    'https://imgur.com/6QDEMJl',
+    'https://imgur.com/s2p2h9I',
+    'https://imgur.com/GzpQONR',
+    'https://imgur.com/04AXj2m',
+    'https://imgur.com/AI3N5Xm',
+
+  ]
+  
+  module.exports = robot => {
+    robot.respond(/dame un saurio/gi, msg => msg.send(msg.random(images)))
+  }
+  
