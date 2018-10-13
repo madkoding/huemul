42515217b5ade0b6bc11d594e2f614b022799649
Jorge Riveros <jorge.riveros@itfb.cl>
2018-10-13 00:00:03 -0300

    correccion en los links de las imagenes

--- scripts/dameundinosaurio.js
+++ scripts/dameundinosaurio.js
@@ -14,26 +14,25 @@
 //   @jriveros
 
 const images = [
-    'https://i.imgur.com/a/M02kVmi',
-    'https://i.imgur.com/a/gY1bS2M',
-    'https://i.imgur.com/a/Q7QibR2',
-    'https://imgur.com/a/3Gq1LYN',
-    'https://imgur.com/a/7IpKONP',
-    'https://imgur.com/a/CinB8DY',
-    'https://imgur.com/9GKaYjd',
-    'https://imgur.com/XCG5pCP',
-    'https://imgur.com/A3waVPT',
-    'https://imgur.com/uvVKy3B',
-    'https://imgur.com/6QDEMJl',
-    'https://imgur.com/s2p2h9I',
-    'https://imgur.com/GzpQONR',
-    'https://imgur.com/04AXj2m',
-    'https://imgur.com/AI3N5Xm',
-    'https://imgur.com/3sHB6Qb',
-    'https://imgur.com/kz7ApVE'
-  ]
-  
-  module.exports = robot => {
-    robot.respond(/dame un saurio/gi, msg => msg.send(msg.random(images)))
-  }
-  
+  'https://i.imgur.com/a/M02kVmi',
+  'https://i.imgur.com/a/gY1bS2M',
+  'https://i.imgur.com/a/Q7QibR2',
+  'https://i.imgur.com/a/3Gq1LYN',
+  'https://i.imgur.com/a/7IpKONP',
+  'https://i.imgur.com/a/CinB8DY',
+  'https://i.imgur.com/9GKaYjd',
+  'https://i.imgur.com/XCG5pCP',
+  'https://i.imgur.com/A3waVPT',
+  'https://i.imgur.com/uvVKy3B',
+  'https://i.imgur.com/6QDEMJl',
+  'https://i.imgur.com/s2p2h9I',
+  'https://i.imgur.com/GzpQONR',
+  'https://i.imgur.com/04AXj2m',
+  'https://i.imgur.com/AI3N5Xm',
+  'https://i.imgur.com/3sHB6Qb',
+  'https://i.imgur.com/kz7ApVE'
+]
+
+module.exports = robot => {
+  robot.respond(/dame un saurio/gi, msg => msg.send(msg.random(images)))
+}
