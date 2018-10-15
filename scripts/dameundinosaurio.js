df1a6a2fec851172a05d15f7acb4f64336c7754c
Jorge Riveros <jorge.riveros@itfb.cl>
2018-10-15 00:14:51 -0300

    ok ahora si. .jpg added and picture links updated.

--- scripts/dameundinosaurio.js
+++ scripts/dameundinosaurio.js
@@ -14,23 +14,23 @@
 //   @jriveros
 
 const images = [
-  'https://i.imgur.com/a/M02kVmi',
-  'https://i.imgur.com/a/gY1bS2M',
-  'https://i.imgur.com/a/Q7QibR2',
-  'https://i.imgur.com/a/3Gq1LYN',
-  'https://i.imgur.com/a/7IpKONP',
-  'https://i.imgur.com/a/CinB8DY',
-  'https://i.imgur.com/9GKaYjd',
-  'https://i.imgur.com/XCG5pCP',
-  'https://i.imgur.com/A3waVPT',
-  'https://i.imgur.com/uvVKy3B',
-  'https://i.imgur.com/6QDEMJl',
-  'https://i.imgur.com/s2p2h9I',
-  'https://i.imgur.com/GzpQONR',
-  'https://i.imgur.com/04AXj2m',
-  'https://i.imgur.com/AI3N5Xm',
-  'https://i.imgur.com/3sHB6Qb',
-  'https://i.imgur.com/kz7ApVE'
+  'https://i.imgur.com/afMcjIv.jpg',
+  'https://i.imgur.com/oQqwHny.jpg',
+  'https://i.imgur.com/MWtrON3.jpg',
+  'https://i.imgur.com/aFyBBfq.jpg',
+  'https://i.imgur.com/sMl5hlF.jpg',
+  'https://i.imgur.com/IQhRGYo.jpg',
+  'https://i.imgur.com/9GKaYjd.jpg',
+  'https://i.imgur.com/XCG5pCP.jpg',
+  'https://i.imgur.com/A3waVPT.jpg',
+  'https://i.imgur.com/uvVKy3B.jpg',
+  'https://i.imgur.com/6QDEMJl.jpg',
+  'https://i.imgur.com/s2p2h9I.jpg',
+  'https://i.imgur.com/GzpQONR.jpg',
+  'https://i.imgur.com/04AXj2m.jpg',
+  'https://i.imgur.com/AI3N5Xm.jpg',
+  'https://i.imgur.com/3sHB6Qb.jpg',
+  'https://i.imgur.com/kz7ApVE.jpg'
 ]
 
 module.exports = robot => {
