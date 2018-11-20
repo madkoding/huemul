d73a0587114e7aa82da447442d75e7899bbf1908
Jorge Epuñan <juanbrujo@gmail.com>
2018-11-20 16:23:46 -0300

    fix(js): varuable name, removed Moment as dependency
    test: `huemul subway`

--- scripts/subway-del-dia.js
+++ scripts/subway-del-dia.js
@@ -1,8 +1,8 @@
 // Description:
-//   Entrega el sandwich de Subway con descuento del dia en Chile
+//   Entrega el sandwich de Subway con descuento del día en Chile
 //
 // Dependencies:
-//   Moment
+//   None
 //
 // Configuration:
 //   None
@@ -13,15 +13,12 @@
 // Author:
 //   @jorgeepunan
 
-const moment = require('moment')
-
-const sandwiches = ['Jamón de Pavo', 'Trío de Carnes', 'Pizza', 'Pollo Apanado', 'B.M.T', 'Costillas BBQ Melt', 'Toscano']
-const dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
+const sandwiches = ['de Jamón de Pavo', 'Trío de Carnes', 'de Pizza', 'de Pollo Apanado', 'B.M.T', 'de Costillas BBQ Melt', 'Toscano']
+const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
 
 module.exports = function(robot) {
   robot.respond(/subway/i, function(res) {
-    const currentWeekDayNum = moment().day()
-    let de = (currentWeekDayNum === 0 || currentWeekDayNum === 2 || currentWeekDayNum === 3 || currentWeekDayNum === 5) ? 'de' : ''
-    res.send(`Hoy *${dias[currentWeekDayNum]}* el sandwich del día en todos los Subway es el ${de} *${sandwiches[currentWeekDayNum]}*. Precio: $1.990 el de 15 cms.`)
+    const currentWeekDayNum = new Date().getDay()
+    res.send(`Hoy *${days[currentWeekDayNum]}* el sandwich del día en todos los Subway es el *${sandwiches[currentWeekDayNum]}*. Precio: $1.990 el de 15 cms.`)
   })
 }
