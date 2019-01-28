9c9c1299e94a8890522ff6f389f9069636eb1d0a
robertodote <roberto.dote@globant.com>
2019-01-28 15:53:41 -0300

    Get popular projects from kickstarter

---
+++ scripts/kickstarter.js
@@ -0,0 +1,46 @@
+// Description:
+//   Show random popular tech projects from kickstarter
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   kickstarter
+//
+// Author:
+//   @edades
+
+module.exports = robot => {
+  robot.respond(/kickstarter (.*)/i, msg => {
+    const term = msg.match[1]
+    const popularTechProjects = `https://www.kickstarter.com/discover/popular?term=${term}&format=json`
+    msg.send(`Cargando los proyectos de *${term}* más populares en Kickstarter :loading:`)
+    robot.http(popularTechProjects).get()((err, res, body) => {
+      if (err || res.statusCode !== 200) {
+        return robot.emit('error', err || new Error(`Status code ${res.statusCode}`), msg)
+      }
+      const jsonData = JSON.parse(body)
+      const projects = jsonData.projects.map(item => ({
+        name: item.name,
+        description: item.blurb,
+        creator: item.creator.name,
+        location: item.location.displayable_name,
+        url: item.urls.web.project
+      }))
+      projects.map(item =>
+        msg.send(`
+						------------------------------------
+						- Nombre: ${item.name}
+						- Descripción: ${item.description}
+						- Creado por: ${item.creator}
+						- Ubicación: ${item.location}
+						- Url: ${item.url}
+						------------------------------------\n
+					`)
+      )
+    })
+  })
+}
