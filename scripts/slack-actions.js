bd1f0dca9baa3f2698e50dc217fa00c5914ddecb
Guillermo Moreno <gmq@guillermomoreno.cl>
2019-03-09 20:27:39 -0300

    feat(slack): agrega ruta para callbacks de slack

---
+++ scripts/slack-actions.js
@@ -0,0 +1,19 @@
+// Description:
+//   Agrega ruta para callbacks de Slack
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Author:
+//   @gmq
+
+module.exports = robot => {
+  robot.router.post(`/slack-actions`, (req, res) => {
+    const payload = JSON.parse(req.body.payload)
+    const action = payload['callback_id'] || payload['actions'][0]['action_id']
+    robot.emit(action, payload)
+  })
+}
