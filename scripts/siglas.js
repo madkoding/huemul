0bf3e0ed49860b461aefee55d00c4238845e9e16
Jorge Epuñan <juanbrujo@gmail.com>
2019-03-13 22:15:20 -0300

    chore(js): improvements
    test: `huemul siglas Jorge Magno`

--- scripts/siglas.js
+++ scripts/siglas.js
@@ -17,36 +17,37 @@ let rand = array => array[Math.floor(Math.random() * array.length)]
 
 module.exports = function(robot) {
   robot.respond(/siglas (.*)/i, function(msg) {
-    let i, len, letter, letters, input, ref, str
+    let i, len, letter, letters, input, ref, str, maxChars
 
     input = msg.match[1]
+    maxChars = 16
 
-    if (input.length > 10) {
-      msg.send('Loco, no weís, máximo 10 caracteres. :loco2:')
+    if (input.length > maxChars) {
+      msg.send(`Loco, no weís, máximo ${maxChars} caracteres. :loco2:`)
     } else {
       letters = {
         a: ['Amoroso', 'Amable', 'Animal', 'Amigo'],
-        b: ['Bello', 'Bellako', 'Bestia', 'Burro', 'Bacán', 'Borracho', 'Bakén'],
+        b: ['Bello', 'Bellako', 'Bestia', 'Burro', 'Bacán', 'Borracho', 'Bakén', 'Beodo'],
         c: ['Cómodo', 'Corredor', 'Camarero', 'Choriflai'],
         d: ['Diputado', 'Dije', 'Desainer'],
         e: ['Enfermo', 'Erótico', 'Ex'],
-        f: ['Fornido', 'Fake', 'Fornido', 'Fronén'],
-        g: ['Güiña', 'Grande', 'Gitano'],
+        f: ['Fornido', 'Fake', 'Fronén', 'Feo'],
+        g: ['Güiña', 'Grande', 'Gitano', 'Glorioso'],
         h: ['Huaso', 'Hipster', 'Haker', 'Humilde'],
-        i: ['Inútil', 'Indigente', 'Iluso'],
+        i: ['Inútil', 'Indigente', 'Iluso', 'Inspirado'],
         j: ['Jarcor', 'jQuery'],
         k: ['Kulero'],
         l: ['Lorea', 'Lolein', 'Lais'],
         m: ['Machucao', 'Maldito', 'Músico', 'Mariwanero'],
-        n: ['Nerd', 'Nudista'],
+        n: ['Nerd', 'Nudista', 'NaN'],
         ñ: ['Ñoño'],
         o: ['Odioso', 'Otaco'],
         p: ['Peante', 'Pollo'],
         q: ['Querido', 'Quizás'],
         r: ['Ronrronero', 'Rellenito', 'Regio'],
         s: ['Sapo', 'Sagaz'],
-        t: ['Tatuado', 'Teletón'],
-        u: ['Uniceja', 'Urbano', 'Unicelular'],
+        t: ['Tatuado', 'Teletón', 'Tetona'],
+        u: ['Uniceja', 'Urbano', 'Unicelular', 'Ulpo'],
         v: ['Vago', 'Vampiro', 'Violento'],
         w: ['Wey', 'Waka Waka'],
         x: ['Xoro'],
