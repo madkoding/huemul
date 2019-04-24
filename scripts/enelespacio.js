966a8cc651943de585fc9aea37bd54b3d5250759
Alvaro Alvarez <alv.ale.alvarez@gmail.com>
2019-04-24 15:44:00 -0400

    agregue try catch por json parse

--- scripts/enelespacio.js
+++ scripts/enelespacio.js
@@ -21,20 +21,24 @@ module.exports = function(robot) {
   robot.respond(/en el espacio/i, res => {
     robot.http(url).get()((error, response, body) => {
       if (!error && response.statusCode === 200) {
-        const data = JSON.parse(body)
-        const cuantos = data.number
-        const message = []
+        try {
+          const data = JSON.parse(body)
+          const cuantos = data.number
+          const message = []
 
-        message.push(`En este momento hay *${cuantos}* personas en el espacio ${res.random(emojis)}`)
+          message.push(`En este momento hay *${cuantos}* personas en el espacio ${res.random(emojis)}`)
 
-        data.people.forEach(d => {
-          const donde = d.craft
-          const quien = d.name
+          data.people.forEach(d => {
+            const donde = d.craft
+            const quien = d.name
 
-          message.push(` · ${quien} (${donde})`)
-        })
+            message.push(` · ${quien} (${donde})`)
+          })
 
-        res.send(...message)
+          res.send(...message)
+        } catch (err) {
+          robot.emit('error', err, res, 'en el espacio')
+        }
       } else {
         res.send(':facepalm: Error: ', error)
       }
