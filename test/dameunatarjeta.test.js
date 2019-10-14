65d27eb8de5c7ec28bd10714d4292bca56688758
David Lay <davidlaym@gmail.com>
2019-10-13 23:47:39 -0300

    fix(uoct) updated events JSON source for new UOCT website structure (#581)
    * fix(uoct) updated events JSON source for new UOCT website structure
    
    * resolving LGTM bot alert
    
    * fix unrelated test to make ci check happy
    
    * removed unnecesary console.log

--- test/dameunatarjeta.test.js
+++ test/dameunatarjeta.test.js
@@ -6,7 +6,15 @@ import nock from 'nock'
 
 const helper = new Helper('../scripts/dameunatarjeta.js')
 const sleep = m => new Promise(resolve => setTimeout(() => resolve(), m))
-
+const nockIsDone = n =>
+  new Promise(resolve => {
+    const poller = setInterval(() => {
+      if (n.isDone()) {
+        clearInterval(poller)
+        resolve()
+      }
+    }, 100)
+  })
 test.beforeEach(t => {
   t.context.room = helper.createRoom({ httpd: false })
 })
@@ -14,11 +22,11 @@ test.beforeEach(t => {
 test.afterEach(t => t.context.room.destroy())
 
 test('Dame una tarjeta via', async t => {
-  nock('http://generatarjetasdecredito.com')
+  const n = nock('https://generatarjetasdecredito.com')
     .get('/generador-tarjetas-visa.php')
     .replyWithFile(200, path.join(__dirname, 'html', 'dameunatarjeta-200.html'))
   t.context.room.user.say('user', 'hubot dame una visa')
-  await sleep(5000)
+  await nockIsDone(n)
 
   const user = t.context.room.messages[0]
   const hubot = t.context.room.messages[1]
--- test/uoct-golden.test.js
+++ test/uoct-golden.test.js
@@ -5,6 +5,15 @@ import nock from 'nock'
 
 const helper = new Helper('../scripts/uoct-golden.js')
 const sleep = m => new Promise(resolve => setTimeout(() => resolve(), m))
+const nockIsDone = m =>
+  new Promise(resolve => {
+    const poller = setInterval(() => {
+      if (nock.isDone()) {
+        clearInterval(poller)
+        resolve()
+      }
+    }, 100)
+  })
 nock.disableNetConnect()
 
 test.beforeEach(t => {
@@ -39,46 +48,116 @@ test('UOCT - cuando no retorna eventos, se responde todo normal', async t => {
     isGold: () => true
   }
 
-  var payload =
-    '{"eventos":[ {"id": 63195,"fecha": "2018-10-10 15:26:10", "interseccion": { "coordenadas": {"lat": "-33.3888202889464","long": "-70.5779708839475"      },      "calle_uno": "Luis Pasteur",      "calle_dos": "Lo Arcaya"    },    "tipo": "Sem\u00e1foros",    "comuna": "Vitacura",    "url": "http://www.uoct.cl/eventos/semforo-apagado-en-luis-pasteur-lo-arcaya/", "informacion": "Sem\u0026aacute;foro apagado en Luis Pasteur / Lo Arcaya"}]}'
+  var payload = '{"response":"ok","data":[]}'
 
   nock('http://www.uoct.cl')
-    .get('/historial/ultimos-eventos/json/')
+    .post('/wp/wp-admin/admin-ajax.php')
+    .times(7)
     .reply(200, payload, { 'content-type': 'text/html; charset=UTF-8' })
 
   t.context.room.user.say('user', 'hubot uoct')
-  await sleep(500)
+  await nockIsDone()
 
   var msglength = t.context.room.messages.length
 
   t.true(msglength >= 3, 'hubot no respondió')
   var responseMsg = t.context.room.messages[2][1]
-  t.is(
-    responseMsg,
-    'Encontrado 1 resultado :bomb::fire:\n15:26: (Vitacura) Semáforo apagado en Luis Pasteur / Lo Arcaya'
-  )
+  t.is(responseMsg, 'Qué raro, parece que está todo normal :thinking_bachelet: . Intenta más tarde.')
 })
+test('UOCT - cuando hay un evento imprime correctamente', async t => {
+  t.context.room.robot.golden = {
+    isGold: () => true
+  }
 
+  var emptyPayload = '{"response":"ok","data":[]}'
+  var payload = `{"data":[{
+    "ID": 128527,
+    "post_author": "5",
+    "post_date": "2019-10-11 20:07:40",
+    "post_date_gmt": "2019-10-11 23:07:40",
+    "post_content": "",
+    "post_title": "A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.",
+    "post_excerpt": "",
+    "post_status": "publish",
+    "comment_status": "closed",
+    "ping_status": "closed",
+    "post_password": "",
+    "post_name": "a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota",
+    "to_ping": "",
+    "pinged": "",
+    "post_modified": "2019-10-12 14:28:26",
+    "post_modified_gmt": "2019-10-12 17:28:26",
+    "post_content_filtered": "",
+    "post_parent": 0,
+    "guid": "http://www.uoct.cl/?post_type=estado_de_transito&#038;p=128527",
+    "menu_order": 0,
+    "post_type": "estado_de_transito",
+    "post_mime_type": "",
+    "comment_count": "0",
+    "filter": "raw",
+    "url": "http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/",
+    "time": "12 · 10 · 2019 - 14:28"
+}]}`
+
+  nock('http://www.uoct.cl')
+    .post('/wp/wp-admin/admin-ajax.php')
+    .reply(200, payload, { 'content-type': 'text/html; charset=UTF-8' })
+
+  nock('http://www.uoct.cl')
+    .post('/wp/wp-admin/admin-ajax.php')
+    .times(6)
+    .reply(200, emptyPayload, { 'content-type': 'text/html; charset=UTF-8' })
+
+  t.context.room.user.say('user', 'hubot uoct')
+  await nockIsDone()
+
+  var msglength = t.context.room.messages.length
+
+  t.true(msglength >= 3, 'hubot no respondió')
+  var responseMsg = t.context.room.messages[2][1]
+  t.true(
+    responseMsg ===
+      'Encontrado 1 resultado :bomb::fire:\n14:28: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.'
+  )
+  return true
+})
 test('UOCT - cuando más de 5, solo muestra 5 ', async t => {
   t.context.room.robot.golden = {
     isGold: () => true
   }
 
-  var payload =
-    '{"eventos":[' +
-    ' {"fecha": "2018-10-10 15:26:10", "comuna": "Vitacura", "url": "http://www.test.com", "informacion": "t&eacute;st1"},' +
-    ' {"fecha": "2018-10-10 15:26:10", "comuna": "Vitacura", "url": "http://www.test.com", "informacion": "t&eacute;st2"},' +
-    ' {"fecha": "2018-10-10 15:26:10", "comuna": "Vitacura", "url": "http://www.test.com", "informacion": "t&eacute;st3"},' +
-    ' {"fecha": "2018-10-10 15:26:10", "comuna": "Vitacura", "url": "http://www.test.com", "informacion": "test4"},' +
-    ' {"fecha": "2018-10-10 15:26:10", "comuna": "Vitacura", "url": "http://www.test.com", "informacion": "test5"},' +
-    ' {"fecha": "2018-10-10 15:26:10", "comuna": "Vitacura", "url": "http://www.test.com", "informacion": "test6"},' +
-    ' {"fecha": "2018-10-10 15:26:10", "comuna": "Vitacura", "url": "http://www.test.com", "informacion": "test7"},' +
-    ' {"fecha": "2018-10-10 15:26:10", "comuna": "Vitacura", "url": "http://www.test.com", "informacion": "test8"},' +
-    ' {"fecha": "2018-10-10 15:26:10", "comuna": "Vitacura", "url": "http://www.test.com", "informacion": "test9"}' +
-    ']}'
+  var payload = `{"data":[{
+    "ID": 128527,
+    "post_author": "5",
+    "post_date": "2019-10-11 20:07:40",
+    "post_date_gmt": "2019-10-11 23:07:40",
+    "post_content": "",
+    "post_title": "(Vitacura) tést",
+    "post_excerpt": "",
+    "post_status": "publish",
+    "comment_status": "closed",
+    "ping_status": "closed",
+    "post_password": "",
+    "post_name": "a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota",
+    "to_ping": "",
+    "pinged": "",
+    "post_modified": "2019-10-12 14:28:26",
+    "post_modified_gmt": "2019-10-12 17:28:26",
+    "post_content_filtered": "",
+    "post_parent": 0,
+    "guid": "http://www.uoct.cl/?post_type=estado_de_transito&#038;p=128527",
+    "menu_order": 0,
+    "post_type": "estado_de_transito",
+    "post_mime_type": "",
+    "comment_count": "0",
+    "filter": "raw",
+    "url": "http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/",
+    "time": "12 · 10 · 2019 - 14:28"
+}]}`
 
   nock('http://www.uoct.cl')
-    .get('/historial/ultimos-eventos/json/')
+    .post('/wp/wp-admin/admin-ajax.php')
+    .times(7)
     .reply(200, payload, { 'content-type': 'text/html; charset=UTF-8' })
 
   t.context.room.user.say('user', 'hubot uoct')
@@ -89,35 +168,14 @@ test('UOCT - cuando más de 5, solo muestra 5 ', async t => {
   t.true(msglength >= 3, 'hubot no respondió')
   var responseMsg = t.context.room.messages[2][1]
   var expectedMessage =
-    'Encontrados 9 resultados :bomb::fire:\n' +
-    '15:26: (Vitacura) tést1\n' +
-    '15:26: (Vitacura) tést2\n' +
-    '15:26: (Vitacura) tést3\n' +
-    '15:26: (Vitacura) test4\n' +
-    '15:26: (Vitacura) test5\n' +
-    '<http://www.uoct.cl/historial/ultimos-eventos/|Ver más resultados>'
+    'Encontrados 7 resultados :bomb::fire:\n' +
+    '14:28: (Vitacura) tést\n' +
+    '14:28: (Vitacura) tést\n' +
+    '14:28: (Vitacura) tést\n' +
+    '14:28: (Vitacura) tést\n' +
+    '14:28: (Vitacura) tést\n' +
+    '<http://www.uoct.cl|Ver más resultados>'
   t.is(responseMsg, expectedMessage)
-})
-test('UOCT - cuando hay un evento imprime correctamente', async t => {
-  t.context.room.robot.golden = {
-    isGold: () => true
-  }
-
-  var payload = '{"eventos":[]}'
-
-  nock('http://www.uoct.cl')
-    .get('/historial/ultimos-eventos/json/')
-    .reply(200, payload, { 'content-type': 'text/html; charset=UTF-8' })
-
-  t.context.room.user.say('user', 'hubot uoct')
-  await sleep(500)
-
-  var msglength = t.context.room.messages.length
-
-  t.true(msglength >= 3, 'hubot no respondió')
-  var responseMsg = t.context.room.messages[2][1]
-  t.true(responseMsg === 'Qué raro, parece que está todo normal :thinking_bachelet: . Intenta más tarde.')
-  return true
 })
 test('UOCT - cuando 404 entrega mensaje de error', async t => {
   t.context.room.robot.golden = {
@@ -125,11 +183,12 @@ test('UOCT - cuando 404 entrega mensaje de error', async t => {
   }
 
   nock('http://www.uoct.cl')
-    .get('/historial/ultimos-eventos/json/')
+    .post('/wp/wp-admin/admin-ajax.php')
+    .times(7)
     .reply(404)
 
   t.context.room.user.say('user', 'hubot uoct')
-  await sleep(500)
+  await nockIsDone()
 
   var msglength = t.context.room.messages.length
 
@@ -137,17 +196,19 @@ test('UOCT - cuando 404 entrega mensaje de error', async t => {
   var responseMsg = t.context.room.messages[2][1]
   t.true(responseMsg === 'Error consultando UOCT: no se pudo obtener eventos')
 })
+
 test('UOCT - cuando 500 entrega mensaje de error', async t => {
   t.context.room.robot.golden = {
     isGold: () => true
   }
 
   nock('http://www.uoct.cl')
-    .get('/historial/ultimos-eventos/json/')
+    .post('/wp/wp-admin/admin-ajax.php')
+    .times(7)
     .reply(500)
 
   t.context.room.user.say('user', 'hubot uoct')
-  await sleep(500)
+  await nockIsDone()
 
   var msglength = t.context.room.messages.length
 
