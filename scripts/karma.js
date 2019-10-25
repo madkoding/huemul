7b16d3b40f6f4f56c2d627268000c3da8b2352a2
Guillermo Moreno <gmq@guillermomoreno.cl>
2019-10-25 20:28:22 -0300

    feat(karma): use ids to compare users

--- scripts/karma.js
+++ scripts/karma.js
@@ -139,7 +139,7 @@ module.exports = robot => {
       userForToken(userToken, response)
         .then(targetUser => {
           if (!targetUser) return
-          if (thisUser.name === getCleanName(targetUser) && op !== '--')
+          if (thisUser.id === targetUser.id && op !== '--')
             return response.send('¡Oe no po, el karma es pa otros no pa ti!')
           if (targetUser.length === '') return response.send('¡Oe no seai pillo, escribe un nombre!')
           const limit = canUpvote(thisUser, targetUser)
