ee7369241240fec22adc86c218ce04d2381fa28b
Leonardo Gatica <lgaticaq@users.noreply.github.com>
2019-11-16 10:49:40 -0300

    feat(uoct): better ouput (#582)

--- scripts/uoct-golden.js
+++ scripts/uoct-golden.js
@@ -13,63 +13,151 @@
 // Author:
 //   @jorgeepunan, @davidlaym
 
-const moment = require('moment')
 const fetch = require('node-fetch')
+const { URLSearchParams } = require('url')
 
-const LIMITE_DE_EVENTOS = 5
+/**
+ * @typedef {Object} Event
+ * @property {number} ID
+ * @property {string} post_author
+ * @property {string} post_date
+ * @property {string} post_date_gmt
+ * @property {string} post_content
+ * @property {string} post_title
+ * @property {string} post_excerpt
+ * @property {string} post_status
+ * @property {string} comment_status
+ * @property {string} ping_status
+ * @property {string} post_password
+ * @property {string} post_name
+ * @property {string} to_ping
+ * @property {string} pinged
+ * @property {string} post_modified
+ * @property {string} post_modified_gmt
+ * @property {string} post_content_filtered
+ * @property {number} post_parent
+ * @property {string} guid
+ * @property {number} menu_order
+ * @property {string} post_type
+ * @property {string} post_mime_type
+ * @property {string} comment_count
+ * @property {string} filter
+ * @property {string} url
+ * @property {string} time
+ */
+/**
+ * @typedef {Object} Response
+ * @property {string} response
+ * @property {Array<Event>} data
+ */
+/**
+ * @param {Event} event
+ * @returns {string}
+ */
+const fallbackText = event => `${event.post_modified}: ${event.post_title}\n`
+
+/**
+ * @param {Event} event
+ * @returns {string}
+ */
+const attachmentText = event => `<${event.url}|${event.post_modified}: ${event.post_title}>\n`
+
+/**
+ * @param {Array<Event>} events
+ * @param {boolean} [fallback=false]
+ * @returns {string}
+ */
+const parseEvents = (events, fallback = false) => {
+  const parser = fallback ? fallbackText : attachmentText
+  return events
+    .sort((firstEvent, secondEvent) => firstEvent.post_modified < secondEvent.post_modified)
+    .reduce((text, event) => {
+      text += parser(event)
+      return text
+    }, '')
+}
 
 module.exports = function (robot) {
   robot.respond(/uoct|taco|tr(aá)nsito/i, function (msg) {
-    function sendError (err, message) {
+    const send = options => {
+      if (['SlackBot', 'Room'].includes(robot.adapter.constructor.name)) {
+        robot.adapter.client.web.chat.postMessage(msg.message.room, null, options)
+      } else {
+        msg.send(options.attachments[0].fallback)
+      }
+    }
+
+    function sendError (err, message, options) {
       if (err) {
         robot.emit('error', err, msg, 'uoct-golden')
       }
-      msg.send('Error consultando UOCT: ' + message)
+      const text = `Error consultando UOCT: ${message}`
+      options.attachments.push({
+        fallback: text,
+        text,
+        title: 'Estado del tránsito'
+      })
+      send(options)
+    }
+
+    const options = {
+      as_user: false,
+      link_names: 1,
+      icon_url: 'https://i.imgur.com/G1cKPWL.png',
+      username: 'UOCT RM',
+      unfurl_links: false,
+      attachments: []
     }
 
     if (!robot.golden.isGold(msg.message.user.name)) {
-      msg.send(
+      const text =
         'Esta funcionalidad es exclusiva para socios golden :monea: de devsChile. Dona en www.devschile.cl para participar de este selecto grupo :huemul-patitas: .'
-      )
-      return
+      options.attachments.push({
+        fallback: text,
+        text,
+        title: 'Estado del tránsito'
+      })
+      return send(options)
     }
-
-    msg.send(
-      'Buscando qué cagá está en esta ciudad :ql:   :walking: :boom: :car: :boom: :blue_car: :boom: :red_car: :boom:          :police_car:'
-    )
 
     const url = 'http://www.uoct.cl/wp/wp-admin/admin-ajax.php'
     const zones = ['sur', 'suroriente', 'surponiente', 'norte', 'nororiente', 'norponiente', 'centro']
-    const requests = zones.map(z => {
-      const params = new URLSearchParams(`action=home_incident_zone&zone=zona-${z}`)
+    const requests = zones.map(zone => {
+      const params = new URLSearchParams(`action=home_incident_zone&zone=zona-${zone}`)
       return fetch(url, { method: 'POST', body: params }).then(res => res.json())
     })
 
     Promise.all(requests)
-      .then(responses => {
-        return responses.reduce((acc, r) => {
-          return acc.concat(r.data)
-        }, [])
+      .then((/** @type {Array<Response>} */ responses) => {
+        return responses
+          .reduce((/** @type {Array<Event>} */ events, event) => {
+            return events.concat(event.data)
+          }, [])
+          .sort((firstEvent, secondEvent) => firstEvent.post_modified < secondEvent.post_modified)
       })
       .then(events => {
         if (events.length === 0) {
-          msg.send('Qué raro, parece que está todo normal :thinking_bachelet: . Intenta más tarde.')
-        } else {
-          const eventList = events
-            .slice(0, LIMITE_DE_EVENTOS)
-            .map(e => `${moment(e.post_modified).format('HH:mm')}: ${e.post_title}`)
-            .join('\n')
-
-          const plural = events.length > 1 ? ['s', 's'] : ['', '']
-          const resume = 'Encontrado' + plural[0] + ' ' + events.length + ' resultado' + plural[1] + ' :bomb::fire:\n'
-          const more = events.length > LIMITE_DE_EVENTOS ? '\n<http://www.uoct.cl|Ver más resultados>' : ''
-          const text = `${resume}${eventList}${more}`
-
-          msg.send(text)
+          const text = 'Qué raro, parece que está todo normal :thinking_bachelet: . Intenta más tarde.'
+          options.attachments.push({
+            fallback: text,
+            text,
+            title: 'Estado del tránsito'
+          })
+          return send(options)
         }
+        const plural = events.length > 1 ? ['s', 's'] : ['', '']
+        const fallbackText = parseEvents(events, true)
+        const text = parseEvents(events)
+        const fallback = `Encontrado${plural[0]} ${events.length} resultado${plural[1]} :bomb::fire:\n${fallbackText}`
+        options.attachments.push({
+          fallback,
+          text,
+          title: 'Estado del tránsito'
+        })
+        send(options)
       })
       .catch(err => {
-        sendError(err, 'no se pudo obtener eventos')
+        sendError(err, 'no se pudo obtener eventos', options)
       })
   })
 }
--- test/uoct-golden.test.js
+++ test/uoct-golden.test.js
@@ -3,6 +3,36 @@ import test from 'ava'
 import Helper from 'hubot-test-helper'
 import nock from 'nock'
 
+const emptyPayload = '{"response":"ok","data":[]}'
+const payload = `{"data":[{
+  "ID": 128527,
+  "post_author": "5",
+  "post_date": "2019-10-11 20:07:40",
+  "post_date_gmt": "2019-10-11 23:07:40",
+  "post_content": "",
+  "post_title": "A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.",
+  "post_excerpt": "",
+  "post_status": "publish",
+  "comment_status": "closed",
+  "ping_status": "closed",
+  "post_password": "",
+  "post_name": "a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota",
+  "to_ping": "",
+  "pinged": "",
+  "post_modified": "2019-10-12 14:28:26",
+  "post_modified_gmt": "2019-10-12 17:28:26",
+  "post_content_filtered": "",
+  "post_parent": 0,
+  "guid": "http://www.uoct.cl/?post_type=estado_de_transito&#038;p=128527",
+  "menu_order": 0,
+  "post_type": "estado_de_transito",
+  "post_mime_type": "",
+  "comment_count": "0",
+  "filter": "raw",
+  "url": "http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/",
+  "time": "12 · 10 · 2019 - 14:28"
+}]}`
+
 const helper = new Helper('../scripts/uoct-golden.js')
 const sleep = m => new Promise(resolve => setTimeout(() => resolve(), m))
 const nockIsDone = m =>
@@ -18,6 +48,19 @@ nock.disableNetConnect()
 
 test.beforeEach(t => {
   t.context.room = helper.createRoom({ httpd: false })
+  t.context.room.robot.adapter.client = {
+    web: {
+      chat: {
+        postMessage: (channel, text, options) => {
+          t.context.postMessage = {
+            channel: channel,
+            text: text,
+            options: options
+          }
+        }
+      }
+    }
+  }
 })
 
 test.afterEach(t => {
@@ -31,16 +74,16 @@ test('UOCT - restringido para usuarios gold', async t => {
   }
   t.context.room.user.say('user', 'hubot uoct')
   await sleep(500)
-
-  var msglength = t.context.room.messages.length
-
-  t.true(msglength >= 2, 'hubot no respondió')
-  var responseMsg = t.context.room.messages[1][1]
-  t.true(
-    responseMsg ===
-      'Esta funcionalidad es exclusiva para socios golden :monea: de devsChile. Dona en www.devschile.cl para participar de este selecto grupo :huemul-patitas: .'
-  )
-  return true
+  t.deepEqual(t.context.room.messages, [['user', 'hubot uoct']])
+  t.deepEqual(t.context.postMessage.options.attachments, [
+    {
+      fallback:
+        'Esta funcionalidad es exclusiva para socios golden :monea: de devsChile. Dona en www.devschile.cl para participar de este selecto grupo :huemul-patitas: .',
+      text:
+        'Esta funcionalidad es exclusiva para socios golden :monea: de devsChile. Dona en www.devschile.cl para participar de este selecto grupo :huemul-patitas: .',
+      title: 'Estado del tránsito'
+    }
+  ])
 })
 
 test('UOCT - cuando no retorna eventos, se responde todo normal', async t => {
@@ -48,112 +91,56 @@ test('UOCT - cuando no retorna eventos, se responde todo normal', async t => {
     isGold: () => true
   }
 
-  var payload = '{"response":"ok","data":[]}'
-
   nock('http://www.uoct.cl')
     .post('/wp/wp-admin/admin-ajax.php')
     .times(7)
-    .reply(200, payload, { 'content-type': 'text/html; charset=UTF-8' })
-
-  t.context.room.user.say('user', 'hubot uoct')
-  await nockIsDone()
-
-  var msglength = t.context.room.messages.length
-
-  t.true(msglength >= 3, 'hubot no respondió')
-  var responseMsg = t.context.room.messages[2][1]
-  t.is(responseMsg, 'Qué raro, parece que está todo normal :thinking_bachelet: . Intenta más tarde.')
-})
-test('UOCT - cuando hay un evento imprime correctamente', async t => {
-  t.context.room.robot.golden = {
-    isGold: () => true
-  }
-
-  var emptyPayload = '{"response":"ok","data":[]}'
-  var payload = `{"data":[{
-    "ID": 128527,
-    "post_author": "5",
-    "post_date": "2019-10-11 20:07:40",
-    "post_date_gmt": "2019-10-11 23:07:40",
-    "post_content": "",
-    "post_title": "A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.",
-    "post_excerpt": "",
-    "post_status": "publish",
-    "comment_status": "closed",
-    "ping_status": "closed",
-    "post_password": "",
-    "post_name": "a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota",
-    "to_ping": "",
-    "pinged": "",
-    "post_modified": "2019-10-12 14:28:26",
-    "post_modified_gmt": "2019-10-12 17:28:26",
-    "post_content_filtered": "",
-    "post_parent": 0,
-    "guid": "http://www.uoct.cl/?post_type=estado_de_transito&#038;p=128527",
-    "menu_order": 0,
-    "post_type": "estado_de_transito",
-    "post_mime_type": "",
-    "comment_count": "0",
-    "filter": "raw",
-    "url": "http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/",
-    "time": "12 · 10 · 2019 - 14:28"
-}]}`
-
-  nock('http://www.uoct.cl')
-    .post('/wp/wp-admin/admin-ajax.php')
-    .reply(200, payload, { 'content-type': 'text/html; charset=UTF-8' })
-
-  nock('http://www.uoct.cl')
-    .post('/wp/wp-admin/admin-ajax.php')
-    .times(6)
     .reply(200, emptyPayload, { 'content-type': 'text/html; charset=UTF-8' })
 
   t.context.room.user.say('user', 'hubot uoct')
   await nockIsDone()
 
-  var msglength = t.context.room.messages.length
-
-  t.true(msglength >= 3, 'hubot no respondió')
-  var responseMsg = t.context.room.messages[2][1]
-  t.true(
-    responseMsg ===
-      'Encontrado 1 resultado :bomb::fire:\n14:28: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.'
-  )
-  return true
+  t.deepEqual(t.context.room.messages, [['user', 'hubot uoct']])
+  t.deepEqual(t.context.postMessage.options.attachments, [
+    {
+      fallback: 'Qué raro, parece que está todo normal :thinking_bachelet: . Intenta más tarde.',
+      text: 'Qué raro, parece que está todo normal :thinking_bachelet: . Intenta más tarde.',
+      title: 'Estado del tránsito'
+    }
+  ])
 })
-test('UOCT - cuando más de 5, solo muestra 5 ', async t => {
+
+test('UOCT - cuando hay un evento imprime correctamente', async t => {
   t.context.room.robot.golden = {
     isGold: () => true
   }
 
-  var payload = `{"data":[{
-    "ID": 128527,
-    "post_author": "5",
-    "post_date": "2019-10-11 20:07:40",
-    "post_date_gmt": "2019-10-11 23:07:40",
-    "post_content": "",
-    "post_title": "(Vitacura) tést",
-    "post_excerpt": "",
-    "post_status": "publish",
-    "comment_status": "closed",
-    "ping_status": "closed",
-    "post_password": "",
-    "post_name": "a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota",
-    "to_ping": "",
-    "pinged": "",
-    "post_modified": "2019-10-12 14:28:26",
-    "post_modified_gmt": "2019-10-12 17:28:26",
-    "post_content_filtered": "",
-    "post_parent": 0,
-    "guid": "http://www.uoct.cl/?post_type=estado_de_transito&#038;p=128527",
-    "menu_order": 0,
-    "post_type": "estado_de_transito",
-    "post_mime_type": "",
-    "comment_count": "0",
-    "filter": "raw",
-    "url": "http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/",
-    "time": "12 · 10 · 2019 - 14:28"
-}]}`
+  nock('http://www.uoct.cl')
+    .post('/wp/wp-admin/admin-ajax.php', body => body.zone !== 'zona-norte')
+    .times(6)
+    .reply(200, emptyPayload, { 'content-type': 'text/html; charset=UTF-8' })
+  nock('http://www.uoct.cl')
+    .post('/wp/wp-admin/admin-ajax.php', body => body.zone === 'zona-norte')
+    .reply(200, payload, { 'content-type': 'text/html; charset=UTF-8' })
+
+  t.context.room.user.say('user', 'hubot uoct')
+  await nockIsDone()
+
+  t.deepEqual(t.context.room.messages, [['user', 'hubot uoct']])
+  t.deepEqual(t.context.postMessage.options.attachments, [
+    {
+      fallback:
+        'Encontrado 1 resultado :bomb::fire:\n2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.\n',
+      text:
+        '<http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/|2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.>\n',
+      title: 'Estado del tránsito'
+    }
+  ])
+})
+
+test('UOCT - cuando más de 5, solo muestra 5 ', async t => {
+  t.context.room.robot.golden = {
+    isGold: () => true
+  }
 
   nock('http://www.uoct.cl')
     .post('/wp/wp-admin/admin-ajax.php')
@@ -163,20 +150,18 @@ test('UOCT - cuando más de 5, solo muestra 5 ', async t => {
   t.context.room.user.say('user', 'hubot uoct')
   await sleep(500)
 
-  var msglength = t.context.room.messages.length
-
-  t.true(msglength >= 3, 'hubot no respondió')
-  var responseMsg = t.context.room.messages[2][1]
-  var expectedMessage =
-    'Encontrados 7 resultados :bomb::fire:\n' +
-    '14:28: (Vitacura) tést\n' +
-    '14:28: (Vitacura) tést\n' +
-    '14:28: (Vitacura) tést\n' +
-    '14:28: (Vitacura) tést\n' +
-    '14:28: (Vitacura) tést\n' +
-    '<http://www.uoct.cl|Ver más resultados>'
-  t.is(responseMsg, expectedMessage)
+  t.deepEqual(t.context.room.messages, [['user', 'hubot uoct']])
+  t.deepEqual(t.context.postMessage.options.attachments, [
+    {
+      fallback:
+        'Encontrados 7 resultados :bomb::fire:\n2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.\n2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.\n2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.\n2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.\n2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.\n2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.\n2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.\n',
+      text:
+        '<http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/|2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.>\n<http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/|2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.>\n<http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/|2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.>\n<http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/|2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.>\n<http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/|2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.>\n<http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/|2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.>\n<http://www.uoct.cl/estado_de_transito/a-las-1000-hrs-del-domingo-se-realizara-corrida-brooks-en-vitacura-habra-contenciones-y-desvios-detalles-del-recorrido-en-la-nota/|2019-10-12 14:28:26: A partir de las 10:00 hrs. de este domingo 13 se realizará Corrida Brooks en Vitacura. Habrá contenciones y desvíos. Detalles del recorrido en la nota.>\n',
+      title: 'Estado del tránsito'
+    }
+  ])
 })
+
 test('UOCT - cuando 404 entrega mensaje de error', async t => {
   t.context.room.robot.golden = {
     isGold: () => true
@@ -190,11 +175,14 @@ test('UOCT - cuando 404 entrega mensaje de error', async t => {
   t.context.room.user.say('user', 'hubot uoct')
   await nockIsDone()
 
-  var msglength = t.context.room.messages.length
-
-  t.true(msglength >= 3, 'hubot no respondió')
-  var responseMsg = t.context.room.messages[2][1]
-  t.true(responseMsg === 'Error consultando UOCT: no se pudo obtener eventos')
+  t.deepEqual(t.context.room.messages, [['user', 'hubot uoct']])
+  t.deepEqual(t.context.postMessage.options.attachments, [
+    {
+      fallback: 'Error consultando UOCT: no se pudo obtener eventos',
+      text: 'Error consultando UOCT: no se pudo obtener eventos',
+      title: 'Estado del tránsito'
+    }
+  ])
 })
 
 test('UOCT - cuando 500 entrega mensaje de error', async t => {
@@ -210,9 +198,12 @@ test('UOCT - cuando 500 entrega mensaje de error', async t => {
   t.context.room.user.say('user', 'hubot uoct')
   await nockIsDone()
 
-  var msglength = t.context.room.messages.length
-
-  t.true(msglength >= 3, 'hubot no respondió')
-  var responseMsg = t.context.room.messages[2][1]
-  t.true(responseMsg === 'Error consultando UOCT: no se pudo obtener eventos')
+  t.deepEqual(t.context.room.messages, [['user', 'hubot uoct']])
+  t.deepEqual(t.context.postMessage.options.attachments, [
+    {
+      fallback: 'Error consultando UOCT: no se pudo obtener eventos',
+      text: 'Error consultando UOCT: no se pudo obtener eventos',
+      title: 'Estado del tránsito'
+    }
+  ])
 })
