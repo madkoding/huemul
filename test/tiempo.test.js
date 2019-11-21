7f422da086800f4266b4c4539b45992ff7f110ed
Raul Hernandez <raulghm@gmail.com>
2019-11-21 18:22:17 -0300

    refactor: cambia tiempo por clima

--- test/tiempo.test.js
+++ test/tiempo.test.js
@@ -4,7 +4,7 @@ import Helper from 'hubot-test-helper'
 import path from 'path'
 import nock from 'nock'
 
-const helper = new Helper('../scripts/clima.js')
+const helper = new Helper('../scripts/tiempo.js')
 const sleep = m => new Promise(resolve => setTimeout(() => resolve(), m))
 
 test.beforeEach(t => {
@@ -13,86 +13,86 @@ test.beforeEach(t => {
 
 test.afterEach(t => t.context.room.destroy())
 
-test('Clima Santiago, Chile', async t => {
+test('Tiempo Santiago, Chile', async t => {
   nock('http://wttr.in')
     .get('/Santiago')
     .query({ m: '' })
-    .replyWithFile(200, path.join(__dirname, 'html', 'clima-200-1.html'))
-  t.context.room.user.say('user', 'hubot clima')
+    .replyWithFile(200, path.join(__dirname, 'html', 'tiempo-200-1.html'))
+  t.context.room.user.say('user', 'hubot tiempo')
   await sleep(500)
 
   const user = t.context.room.messages[0]
   const hubot = t.context.room.messages[1]
 
-  t.deepEqual(user, ['user', 'hubot clima'])
+  t.deepEqual(user, ['user', 'hubot tiempo'])
   t.is(hubot[0], 'hubot')
   t.true(/Weather report: Santiago, Chile/ig.test(hubot[1]))
 })
 
-test('Clima Paris, France', async t => {
+test('Tiempo Paris, France', async t => {
   nock('http://wttr.in')
     .get('/paris')
     .query({ m: '' })
-    .replyWithFile(200, path.join(__dirname, 'html', 'clima-200-2.html'))
-  t.context.room.user.say('user', 'hubot clima paris')
+    .replyWithFile(200, path.join(__dirname, 'html', 'tiempo-200-2.html'))
+  t.context.room.user.say('user', 'hubot tiempo paris')
   await sleep(500)
 
   const user = t.context.room.messages[0]
   const hubot = t.context.room.messages[1]
 
-  t.deepEqual(user, ['user', 'hubot clima paris'])
+  t.deepEqual(user, ['user', 'hubot tiempo paris'])
   t.is(hubot[0], 'hubot')
   t.true(/Weather report: Paris, France/ig.test(hubot[1]))
 })
 
-test('Clima Error 500', async t => {
+test('Tiempo Error 500', async t => {
   nock('http://wttr.in')
     .get('/Santiago')
     .query({ m: '' })
     .reply(500)
-  t.context.room.user.say('user', 'hubot clima')
+  t.context.room.user.say('user', 'hubot tiempo')
   await sleep(500)
 
   const user = t.context.room.messages[0]
   const hubot = t.context.room.messages[1]
 
-  t.deepEqual(user, ['user', 'hubot clima'])
+  t.deepEqual(user, ['user', 'hubot tiempo'])
   t.deepEqual(hubot, [
     'hubot',
     '@user ocurrió un error con la búsqueda'
   ])
 })
 
-test('Clima 301', async t => {
+test('Tiempo 301', async t => {
   nock('http://wttr.in')
     .get('/Santiago')
     .query({ m: '' })
     .reply(301)
-  t.context.room.user.say('user', 'hubot clima')
+  t.context.room.user.say('user', 'hubot tiempo')
   await sleep(500)
 
   const user = t.context.room.messages[0]
   const hubot = t.context.room.messages[1]
 
-  t.deepEqual(user, ['user', 'hubot clima'])
+  t.deepEqual(user, ['user', 'hubot tiempo'])
   t.deepEqual(hubot, [
     'hubot',
     '@user ocurrió un error con la búsqueda'
   ])
 })
 
-test('Clima request error', async t => {
+test('Tiempo request error', async t => {
   nock('http://wttr.in')
     .get('/Santiago')
     .query({ m: '' })
-    .replyWithFile(200, path.join(__dirname, 'html', 'clima-500.html'))
-  t.context.room.user.say('user', 'hubot clima')
+    .replyWithFile(200, path.join(__dirname, 'html', 'tiempo-500.html'))
+  t.context.room.user.say('user', 'hubot tiempo')
   await sleep(500)
 
   const user = t.context.room.messages[0]
   const hubot = t.context.room.messages[1]
 
-  t.deepEqual(user, ['user', 'hubot clima'])
+  t.deepEqual(user, ['user', 'hubot tiempo'])
   t.deepEqual(hubot, [
     'hubot',
     '@user ocurrió un error con la búsqueda'
