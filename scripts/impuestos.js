1b7ff6a673087d707083d0f41a1603f8668c0dc5
Leonardo Gatica <lgaticaq@users.noreply.github.com>
2020-12-12 18:56:17 -0400

    Issue/652 (#654)
    * docs(impuestos): se reemplaza "all" por "al"
    
    * docs(impuestos): se indican las dependencias del script
    
    * feat(impuestos): se admiten separadores de miles y decimales
    
    Ademas permite el uso del signo "$".
    
    fix #652

--- scripts/impuestos.js
+++ scripts/impuestos.js
@@ -2,13 +2,14 @@
 //   Dice cuanto tienes que pagarle al SII por impuestos y cotizaciones
 //
 // Dependencies:
-//   None
+//   numbertoclpformater
+//   tax-cl
 //
 // Configuration:
 //   None
 //
 // Commands:
-//   hubot impuestos <sueldoMensual> - Dice cuanto tendrás que pagarle all SII en la próxima operación renta
+//   hubot impuestos <sueldoMensual> - Dice cuanto tendrás que pagarle al SII en la próxima operación renta
 //
 // Author:
 //   Nicolás Gómez <ngomez@hey.com>
@@ -16,10 +17,26 @@
 const { calcular, obtenerTramoImpositivo } = require('tax-cl')
 const { numberToCLPFormater } = require('numbertoclpformater')
 
-module.exports = robot => {
-  robot.respond(/impuestos (\d+)$/i, msg => {
-    const income = parseInt(msg.match[1], 10)
+/**
+ * Convierte currency string a numero.
+ *
+ * @param {string} stringNumber -
+ * @returns {number} -
+ */
+const parseLocaleNumber = stringNumber => {
+  const thousandSeparator = '.'
+  const decimalSeparator = ','
 
+  return parseFloat(stringNumber
+    .replace(new RegExp('\\' + thousandSeparator, 'g'), '')
+    .replace(new RegExp('\\' + decimalSeparator), '.')
+  )
+}
+
+module.exports = robot => {
+  robot.respond(/impuestos \$?\s?(\d+[.,\dk]+)$/i, msg => {
+    const rawIncome = msg.match[1]
+    const income = parseLocaleNumber(rawIncome.toString())
     const {
       deuda,
       retencion,
@@ -31,11 +48,11 @@ module.exports = robot => {
 
     const tramoImpositivo = obtenerTramoImpositivo(sueldoTributable)
     const emoji = tramoImpositivo.factor > 0.04 ? ':patrones:' : ':c3:'
-
+    const prefix = `Para el sueldo ${numberToCLPFormater(income)}`
     const messages = [
       deuda > 0
-        ? `Tendrás que pagar ~ *${numberToCLPFormater(deuda)}* ${emoji}`
-        : `Recibirás una devolución de *${numberToCLPFormater(-deuda)}*`,
+        ? `${prefix} tendrás que pagar ~ *${numberToCLPFormater(deuda)}* ${emoji}`
+        : `${prefix} recibirás una devolución de *${numberToCLPFormater(-deuda)}*`,
       `A favor tienes tu retención de boletas (10.75%): *${numberToCLPFormater(retencion)}*`,
       `En contra tienes los impuestos (*${numberToCLPFormater(impuestos)}*) y las cotizaciones (*${numberToCLPFormater(cotizaciones)}*)`,
       parcial > 0
